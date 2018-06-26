3cfc3253d510b06410dc80cd38da2cf1c54df69b
Dimitar Goshevski <roundrobine@gmail.com>
2018-06-26 10:40:23 +0200

    Initial commit

---
+++ src/main/java/com/roundrobine/chatwebsocketapp/ChatWebsocketAppApplication.java
@@ -0,0 +1,12 @@
+package com.roundrobine.chatwebsocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebsocketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWebsocketAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/roundrobine/chatwebsocketapp/ChatWebsocketAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roundrobine.chatwebsocketapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatWebsocketAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
