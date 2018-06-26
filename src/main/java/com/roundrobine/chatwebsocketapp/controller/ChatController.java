8a929b0df8a9a7a994908d810333ebafbe6aa3fe
Dimitar Goshevski <roundrobine@gmail.com>
2018-06-26 17:08:43 +0200

    implement add username to session message mapping

--- src/main/java/com/roundrobine/chatwebsocketapp/controller/ChatController.java
+++ src/main/java/com/roundrobine/chatwebsocketapp/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.roundrobine.chatwebsocketapp.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,7 +12,16 @@ public class ChatController {
 
     @MessageMapping("/send/message")
     @SendTo("/chat/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+    public ChatMessage sendMessage(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/chat/public")
+    public ChatMessage addUser(ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 
