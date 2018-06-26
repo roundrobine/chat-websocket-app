ab1174251ce766748a717da8cfc7a300bca27e62
roundrobine <roundrobine@gmail.com>
2018-06-26 23:08:09 +0200

    add Component annotation

--- src/main/java/com/roundrobine/chatwebsocketapp/events/LiveChatEventListener.java
+++ src/main/java/com/roundrobine/chatwebsocketapp/events/LiveChatEventListener.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -16,6 +17,7 @@ import java.util.Date;
  **/
 
 @Slf4j
+@Component
 public class LiveChatEventListener {
 
     private final SimpMessageSendingOperations simpMessageSendingOperations;
