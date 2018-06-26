e825cf5a95b4de9ce5a2f5e11f9387f763a6d417
Dimitar Goshevski <roundrobine@gmail.com>
2018-06-26 11:51:46 +0200

    design chat message object

---
+++ src/main/java/com/roundrobine/chatwebsocketapp/model/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.roundrobine.chatwebsocketapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ src/main/java/com/roundrobine/chatwebsocketapp/model/MessageType.java
@@ -0,0 +1,6 @@
+package com.roundrobine.chatwebsocketapp.model;
+
+public enum MessageType {
+
+    CHAT, JOIN, LEAVE
+}
