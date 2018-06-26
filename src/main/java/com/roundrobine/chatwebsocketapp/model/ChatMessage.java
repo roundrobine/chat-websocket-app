2362f2604aa9db219bdaecff97c1f0b6b678fc17
roundrobine <roundrobine@gmail.com>
2018-06-26 22:57:44 +0200

    extend chatMessage with a timestamp

--- src/main/java/com/roundrobine/chatwebsocketapp/model/ChatMessage.java
+++ src/main/java/com/roundrobine/chatwebsocketapp/model/ChatMessage.java
@@ -4,6 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+/**
+ * created by roundrobine on  Jun, 2018
+ **/
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,5 +18,6 @@ public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+    private Date timestamp;
 
 }
