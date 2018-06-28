8dda4f330e2854c72f3e5fc9d7c0b20eafe02258
roundrobine <roundrobine@gmail.com>
2018-06-28 16:51:50 +0200

    implement the frontend logic using Vanilla js, socket.js, stomp.js, moment.js libraries and bootstrap css

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const signupPage = document.querySelector('#signup');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#usernameForm');
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const messageArea = document.querySelector('#messageArea');
+const connectingElement = document.querySelector('.connecting');
+
+let stompClient = null;
+let username = null;
+
+const colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function connect(event) {
+    username = document.querySelector('#name').value.trim();
+
+    if(username) {
+        signupPage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        let socket = new SockJS('/websocket');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+    event.preventDefault();
+}
+
+
+function onConnected() {
+    // Subscribe to the chat
+    stompClient.subscribe('/chat/broadcast/', onMessageReceived);
+
+    // record your username
+    stompClient.send("/app/addUser/",
+        {},
+        JSON.stringify({sender: username, type: 'JOIN', timestamp: new Date() })
+    );
+
+    connectingElement.classList.add('hidden');
+}
+
+
+function onError(error) {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
+
+
+function sendMessage(event) {
+    let messageContent = messageInput.value.trim();
+    if(messageContent && stompClient) {
+        let chatMessage = {
+            sender: username,
+            content: messageInput.value,
+            type: 'CHAT',
+            timestamp: new Date()
+        };
+        stompClient.send("/app/send/message/", {}, JSON.stringify(chatMessage));
+        messageInput.value = '';
+    }
+    event.preventDefault();
+}
+
+
+function onMessageReceived(payload) {
+    let message = JSON.parse(payload.body);
+
+    let messageElement = document.createElement('li');
+    messageElement.classList.add('event-message');
+
+    if(message.type === 'JOIN') {
+        message.content = message.sender + ' joined the chat!';
+    } else if (message.type === 'LEAVE') {
+        message.content = message.sender + ' left the chat!';
+    } else {
+        messageElement.classList.remove('event-message');
+        messageElement.classList.add('chat-message');
+        let avatarElement = document.createElement('i');
+        let avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        let usernameElement = document.createElement('span');
+        let usernameText = document.createTextNode(message.sender + ' @ '
+            + moment(message.timestamp).format("HH:mm:ss"));
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    let textElement = document.createElement('p');
+    let messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+
+function getAvatarColor(messageSender) {
+    let hash = 0;
+    for (let i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+    let index = Math.abs(hash % colors.length);
+    return colors[index];
+}
+
+usernameForm.addEventListener('submit', connect, true);
+messageForm.addEventListener('submit', sendMessage, true);
